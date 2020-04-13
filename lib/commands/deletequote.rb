6433068f37b6fff7b0a81073acffa7ca8e01d30c
Ryuujinx <admin@failverse.com>
2020-04-13 13:28:26 -0500

    end

--- lib/commands/deletequote.rb
+++ lib/commands/deletequote.rb
@@ -1,43 +1,44 @@
 class Commands
   def deletequote(bot)
     bot.command(:deletequote, min_args:1, max_args:1) do |event, *args|
-    qid = args.join(' ')
-    yml = YAML.load_file 'data/quotes.yml'
-    quote = yml[qid.to_i]
-    yike_emoji = bot.emoji(489347848450867210)
-    pika_emoji = bot.emoji(524010864467116048)
-    message = event.respond("Delete quote?\n #{qid}: #{quote}\n#{yike_emoji} to confirm, #{pika_emoji} to cancel.")
-    message.react yike_emoji
-    message.react pika_emoji
-    while true
-      reaction = bot.add_await!(Discordrb::Events::ReactionAddEvent, {emoji: [489347848450867210, 524010864467116048]})
-      if message == reaction.message
-        break
-      end
-    end
-    if reaction.emoji == yike_emoji
+      qid = args.join(' ')
       yml = YAML.load_file 'data/quotes.yml'
-      if yml[qid.to_i] == quote
-        yml.delete(qid.to_i)
-        File.open("data/quotes.yml", "w") do |file|
-          file.write(yml.to_yaml)
+      quote = yml[qid.to_i]
+      yike_emoji = bot.emoji(489347848450867210)
+      pika_emoji = bot.emoji(524010864467116048)
+      message = event.respond("Delete quote?\n #{qid}: #{quote}\n#{yike_emoji} to confirm, #{pika_emoji} to cancel.")
+      message.react yike_emoji
+      message.react pika_emoji
+      while true
+        reaction = bot.add_await!(Discordrb::Events::ReactionAddEvent, {emoji: [489347848450867210, 524010864467116048]})
+        if message == reaction.message
+          break
         end
-        yml = YAML.load_file 'data/quotes.yml'
-        yml2 = {}
-        File.open("data/quotes.yml", "w") do |file|
-          yml.keys.each_with_index do |var,i|
-            yml2[i] = yml[var]
-          end
-          file.write(yml2.to_yaml)
-        end
-        event.respond "Quote #{qid} Deleted."
-      else
-        event.respond "DB Changed! Deletion canceled, try again."
       end
-      message.delete
-    elsif reaction.emoji == pika_emoji
-        event.respond "Delete Canceled."
+      if reaction.emoji == yike_emoji
+        yml = YAML.load_file 'data/quotes.yml'
+        if yml[qid.to_i] == quote
+          yml.delete(qid.to_i)
+          File.open("data/quotes.yml", "w") do |file|
+            file.write(yml.to_yaml)
+          end
+          yml = YAML.load_file 'data/quotes.yml'
+          yml2 = {}
+          File.open("data/quotes.yml", "w") do |file|
+            yml.keys.each_with_index do |var,i|
+              yml2[i] = yml[var]
+            end
+            file.write(yml2.to_yaml)
+          end
+          event.respond "Quote #{qid} Deleted."
+        else
+          event.respond "DB Changed! Deletion canceled, try again."
+        end
         message.delete
+      elsif reaction.emoji == pika_emoji
+          event.respond "Delete Canceled."
+          message.delete
+      end
     end
   end
 end
