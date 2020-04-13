8c8d80befe524c1cf4e4884d009e85338861b270
Ryuujinx <admin@failverse.com>
2020-04-13 13:31:53 -0500

    tabs

--- lib/commands/achallengerappears.rb
+++ lib/commands/achallengerappears.rb
@@ -1,6 +1,7 @@
 class Commands
   def achallengerappears(bot)     
     bot.command(:achallengerappears) do |event|
-    event.respond "https://imgur.com/1Mc9tU2"
+      event.respond "https://imgur.com/1Mc9tU2"
+    end
   end
 end
