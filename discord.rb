3a0f87f648c0201380f9c529bb04cc0ba75588b5
Ryuujinx <admin@failverse.com>
2020-04-13 14:17:43 -0500

    Refactors

--- discord.rb
+++ discord.rb
@@ -8,7 +8,6 @@ require 'uri'
 require 'aws-sdk-s3'
 Dir["lib/*.rb"].each {|file| load file}
 Dir["lib/commands/*.rb"].each {|file| load file}
-Dir["lib/config/*.rb"].each {|file| load file}
 Dir.mkdir('tmp') unless Dir.exists?('tmp')
 
 
@@ -16,7 +15,7 @@ Dir.mkdir('tmp') unless Dir.exists?('tmp')
 
 bot = Discordrb::Commands::CommandBot.new(token: DISCORD_TOKEN, client_id: DISCORD_CLIENT_ID, prefix: PREFIX)
 
-cmd = Bot.Commands.new
+cmd = Commands.new
 Commands.instance_methods(false).each do |x|
   cmd.send(x, bot)
 end
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,6 @@
 class Bot
+  load 'lib/random.rb'
+  load 'lib/config/config.rb'
   @@r = Rand.new(YAML.load_file('data/quotes.yml').size)
   @@s3 = Aws::S3::Client.new(profile: S3PROFILE, region: S3REGION)
 end
