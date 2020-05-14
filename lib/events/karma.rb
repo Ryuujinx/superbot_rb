36630d311fd2d6b46fd476ab6b1face24748a20c
Ryuujinx <admin@failverse.com>
2020-05-14 11:42:38 -0500

    Karma stuff

--- lib/events/karma.rb
+++ lib/events/karma.rb
@@ -1,9 +1,22 @@
 class Events < Bot
   def karma_change(bot)  
     bot.message(with_text: /\<@!\d+>\s?(?:\+\+|--)\s?$/) do |event|
-      matchdata = /(?<user>\<@!\d+>)\s?(?<op>\+\+|--)\s?$/.match(event.text)  #There's no way to get match from the first regex without doing gross things, so we check here.
-      event.respond "<@!#{event.user.id}> is trying to change #{matchdata[:user]} karma: #{matchdata[:op]}"
-      puts matchdata[:user]
+      matchdata = /(?<user>\<@!\d+>)\s?(?<op>\+\+|--)\s?$/.match(event.text)  #There's no way to get match from the first regex without doing gross things like using globals, so we check here.
+      if matchdata[:user] != "<@!#{event.user.id}>" #Don't allow users to modify their own karma.
+        yml = YAML.load_file 'data/quotes.yml'
+        if matchdata[:op] == "++"
+          yml[matchdata[:user]] += 1
+          event.respond "<@!#{event.user.id}> gave #{matchdata[:user]} karma! New karma total: #{yml[matchdata[:user]]}"
+        elsif matchdata[:op] == "--"
+          yml[matchdata[:user]] -= 1
+          event.respond "<@!#{event.user.id}> removed karma from #{matchdata[:user]}! New karma total: #{yml[matchdata[:user]]}"
+        else
+          event.respond "You should never see this, if you are then contact the bot owner." #We check for ++/--, we should never hit this branch.
+        end
+        event.respond "<@!#{event.user.id}> is trying to change #{matchdata[:user]} karma: #{matchdata[:op]}"
+      else
+        event.respond "You can't change your own karma!"
+      end
     end
   end
 end
