1b637c87d9a6e71aed8c5c0f9e1d69473bee7f71
Ryuujinx <admin@failverse.com>
2020-05-14 11:28:07 -0500

    Close the interpolation idiot

--- lib/events/karma.rb
+++ lib/events/karma.rb
@@ -2,7 +2,7 @@ class Events < Bot
   def karma_change(bot)  
     bot.message(with_text: /\<@!\d+>\s?(?:\+\+|--)\s?$/) do |event|
       matchdata = /(?<user>\<@!\d+>)\s?(?<op>\+\+|--)\s?$/.match(event.text)  #There's no way to get match from the first regex without doing gross things, so we check here.
-      event.respond "#{event.user} is trying to change #{matchdata[:user]} karma: #{matchdata[:op]"
+      event.respond "#{event.user} is trying to change #{matchdata[:user]} karma: #{matchdata[:op]}"
     end
   end
 end
