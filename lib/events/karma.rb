91e634162666507383c9dab844b2f00980ff467f
Ryuujinx <admin@failverse.com>
2020-05-14 11:51:11 -0500

    Comments, fixed no user case

--- lib/events/karma.rb
+++ lib/events/karma.rb
@@ -6,17 +6,19 @@ class Events < Bot
         yml = YAML.load_file 'data/karma.yml'
         if matchdata[:op] == "++"
           puts yml[matchdata[:user]]
-          yml[matchdata[:user]] += 1
+          yml[matchdata[:user]].nil? ? yml[matchdata[:user]] = 1 : yml[matchdata[:user]] += 1  #If the user doesn't exist, set them to 1.
           event.respond "<@!#{event.user.id}> gave #{matchdata[:user]} karma! New karma total: #{yml[matchdata[:user]]}"
         elsif matchdata[:op] == "--"
-          yml[matchdata[:user]] -= 1
+          yml[matchdata[:user]].nil? ? yml[matchdata[:user]] = -1 : yml[matchdata[:user]] -= 1 #If the user doesn't exist, set them to -1.
           event.respond "<@!#{event.user.id}> removed karma from #{matchdata[:user]}! New karma total: #{yml[matchdata[:user]]}"
         else
           event.respond "You should never see this, if you are then contact the bot owner." #We check for ++/--, we should never hit this branch.
         end
-        event.respond "<@!#{event.user.id}> is trying to change #{matchdata[:user]} karma: #{matchdata[:op]}"
+        File.open('data/karma.yml', 'w') do |file| #Save that shit.
+          file.write(yml.to_yaml)
+        end
       else
-        event.respond "You can't change your own karma!"
+        event.respond "You can't change your own karma!" #Because like, duh. 
       end
     end
   end
