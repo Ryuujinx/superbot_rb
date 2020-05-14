7aa1111c74958b005f55a017b345ceff6371286b
Ryuujinx <admin@failverse.com>
2020-05-14 10:46:03 -0500

    more testing

--- lib/events/karma.rb
+++ lib/events/karma.rb
@@ -1,7 +1,8 @@
 class Events < Bot
   def karma_change(bot)  
-    bot.message(with_text: /test.*/) do |event|
+    bot.message(with_text: /(?<user>\<@!\d+>|\w+)\s?(?<op>\+\+|--)\s?$/) do |event, match|
       puts event.text
+      puts match[:user]
       event.respond event.text
     end
   end
